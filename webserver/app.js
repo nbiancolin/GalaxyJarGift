a50f98d25b9992d18efe80b348829408f5781edf
Nathan Chin <spoink2022@gmail.com>
2023-07-05 21:51:50 -0400

    bug fix

--- webserver/app.js
+++ webserver/app.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://35.193.67.238:80"
+    origin: "http://localhost:8080"
   }
 });
 
