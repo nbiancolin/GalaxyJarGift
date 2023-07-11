acac0d6e6881a53ff671fc7efb1e1ba7b5e772d2
Nathan Chin <spoink2022@gmail.com>
2023-07-11 19:38:47 -0400

    make CORS work

--- webserver/app.js
+++ webserver/app.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:8080"
+    origin: true
   }
 });
 
